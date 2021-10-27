6cb7710e5483755962ec8dd77d9218e1ad5d0e5e
wjiyeon <medi20001@gmail.com>
2021-10-27 17:33:35 +0900

    1-6 변수 선언 문제 및 답

---
+++ wjy/src/day1/DayEx1.java
@@ -0,0 +1,26 @@
+package day1;
+
+public class DayEx1 {
+
+	public static void main(String[] args) {
+		/* 핸드폰 번호를 - 없이 저장하여 관리하려 한다. 알맞게 변수를 선언하고
+		 * 초기값으로 01012345671을 저장하여 확인해보세요.
+		 * */
+		String phone = "01012345671";
+		System.out.println(phone);
+		/* 성별을 저장하여 관리하려 한다. 알맞은 변수를 선언하고 초기값으로 남자를 설정해보세요.
+		 * 단, 성별은 남자와 여자만 있다고 가정.
+		 */
+		String num1 = "male";
+		String num2 = "girl";
+		System.out.println(num1+num2);
+		
+		boolean isMale = true;
+		String gender = "male";
+		int isMale2 = 13;//13:남자, 24:여자라는 걸 기억해야하는 불편함이 있다.
+		
+		
+
+	}
+
+}
